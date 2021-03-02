b203dcfa3b274aa7556bf0a2fc712ed8f551401e
Daniel Bjerkehagen <danielbb@stud.ntnu.no>
2021-03-02 11:36:23 +0100

    Almost complete prework 4, implement simulink

--- normal_hill_constraint.m
+++ normal_hill_constraint.m
@@ -1,4 +1,4 @@
-function [c, ceq] = normal_hill_constrain(x)
+function [c, ceq] = normal_hill_constraint(x)
     alpha = 0.2;
     beta = 20;
     lambda_t = 2*pi/3;
@@ -6,6 +6,6 @@ function [c, ceq] = normal_hill_constrain(x)
     lambda_k = x(1);
     e_k = x(5);
     
-    c = alpha * exp(-beta*(lambda_k - lambda_t)^2)) - e_k;
-    ceq = 0;
+    c = (alpha * exp(-beta*((lambda_k - lambda_t)^2))) - e_k;
+    ceq = [];
 end
--- optimization_layer_pitch_and_elev.m
+++ optimization_layer_pitch_and_elev.m
@@ -56,15 +56,17 @@ Q = gen_q(Q1,P1,N,M);                                  % Generate Q, hint: gen_q
 c = zeros(size(Q,1),1);                                  % Generate c, this is the linear constant term in the QP
 
 %% Generate system matrixes for linear model
-Aeq = gen_aeq(A1,B1,N,mx,mu);             % Generate A, hint: gen_aeq
-beq = [A1*x0; zeros(mx*(N-1),1)];             % Generate b
+Aeq = gen_aeq(A1,B1,N,mx,mu);             %  Generate A, hint: gen_aeq
+beq = [A1*x0; zeros(mx*(N-1),1)];             % enerate b
 
-%% Solve QP problem with linear model
+%% Solve nonlinear problem with linear model
 tic
-z = fmincon(@(z)z'*Q*z, z0, [], [], Aeq, beq, vlb, vub, @normal_hill_constraint(z));
+opts = optimoptions('fmincon','Algorithm','sqp');
+nonlcon = @normal_hill_constraint;
+z = fmincon(@(z)z'*Q*z, z0, [], [], Aeq, beq, vlb, vub, nonlcon, opts);
 t1=toc;
 
-Calculate objective value
+% Calculate objective value
 phi1 = 0.0;
 PhiOut = zeros(N*mx+M*mu,1);
 for i=1:N*mx+M*mu
@@ -73,43 +75,65 @@ for i=1:N*mx+M*mu
 end
 
 %% Extract control inputs and states
-u  = [z(N*mx+1:N*mx+M*mu);z(N*mx+M*mu)]; % Control input from solution
+u1  = [z(N*mx+1:2:N*mx+M*mu);z(N*mx+M*mu)]; % Control input from solution
+u2  = [z(N*mx+2:2:N*mx+M*mu);z(N*mx+M*mu)];
 
 x1 = [x0(1);z(1:mx:N*mx)];              % State x1 from solution
 x2 = [x0(2);z(2:mx:N*mx)];              % State x2 from solution
 x3 = [x0(3);z(3:mx:N*mx)];              % State x3 from solution
 x4 = [x0(4);z(4:mx:N*mx)];              % State x4 from solution
+x5 = [x0(5);z(5:mx:N*mx)];
+x6 = [x0(6);z(6:mx:N*mx)];
 
 num_variables = 5/delta_t;
 zero_padding = zeros(num_variables,1);
 unit_padding  = ones(num_variables,1);
 
-u   = [zero_padding; u; zero_padding];
+u1  = [zero_padding; u1; zero_padding];
+u2  = [zero_padding; u2; zero_padding];
 x1  = [pi*unit_padding; x1; zero_padding];
 x2  = [zero_padding; x2; zero_padding];
 x3  = [zero_padding; x3; zero_padding];
 x4  = [zero_padding; x4; zero_padding];
+x5  = [zero_padding; x5; zero_padding];
+x6  = [zero_padding; x6; zero_padding];
 
 %% Plotting
-t = 0:delta_t:delta_t*(length(u)-1);
+t = 0:delta_t:delta_t*(length(u1)-1);
 
 figure(2)
-subplot(511)
-stairs(t,u),grid
-ylabel('u')
+subplot(811)
+stairs(t,u1),grid
+ylabel('u1')
 title('Optimal trajectory from x_0 to x_f with weight q = 1')
-subplot(512)
+
+subplot(812)
+stairs(t,u2),grid
+ylabel('u1')
+
+subplot(813)
 plot(t,x1,'m',t,x1,'mo'),grid
 ylabel('lambda')
-subplot(513)
+
+subplot(814)
 plot(t,x2,'m',t,x2','mo'),grid
 ylabel('r')
-subplot(514)
+
+subplot(815)
 plot(t,x3,'m',t,x3,'mo'),grid
 ylabel('p')
-subplot(515)
+
+subplot(816)
 plot(t,x4,'m',t,x4','mo'),grid
 xlabel('tid (s)'),ylabel('pdot')
+
+subplot(817)
+plot(t,x5,'m',t,x5','mo'),grid
+xlabel('tid (s)'),ylabel('e')
+
+subplot(818)
+plot(t,x6,'m',t,x6','mo'),grid
+xlabel('tid (s)'),ylabel('edot')
 
 
 %% Plot measurement against calculations
@@ -160,14 +184,17 @@ legend('Measured state', 'Theoretical state')
 % print('p2t7_compare-xopt-xm_Q-[10,5,3,1],R-[0d5]','-dpng');
 
 %% LQ Problem
-u_opt = timeseries(u,t);
+u1_opt = timeseries(u1,t);
+u2_opt = timeseries(u2,t);
 x1_opt = timeseries(x1,t);
 x2_opt = timeseries(x2,t);
 x3_opt = timeseries(x3,t);
 x4_opt = timeseries(x4,t);
-x_opt = [x1_opt; x2_opt; x3_opt; x4_opt];
+x5_opt = timeseries(x5,t);
+x6_opt = timeseries(x6,t);
+x_opt = [x1_opt; x2_opt; x3_opt; x4_opt; x5_opt; x6_opt];
 
-Q_k = diag([10 5 1 1]);
-R_k = 0.5;
+Q_k = diag([10 5 1 1 1 1]);
+R_k = diag([0.5, 1]);
 
 [K,S,e] = dlqr(A,B,Q_k,R_k);
